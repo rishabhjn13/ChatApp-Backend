0e25c1d2085f7f29e229173e268529f81ad005ab
Rishabh Jain <randomboiii069@gmail.com>
2026-01-28 23:18:51 +0530

    Refactor server startup logic and ensure default port is set

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
@@ -17,7 +15,8 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://chat-app-frontend-rho-six.vercel.app",
 ];
-const PORT = process.env.PORT;
+
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(express.json());
@@ -47,7 +46,16 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
-  connectDB();
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    server.listen(PORT, () => {
+      console.log("Server is running on PORT: " + PORT);
+    });
+  } catch (err) {
+    console.error("Failed to start server:", err);
+    process.exit(1);
+  }
+};
+
+startServer();
